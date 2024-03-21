3b3e403d5287341b842fd02683e877de3e23ef5a
parwez <parwezhaidar555@gmail,com>
2024-03-22 04:15:41 +0530

    Second Change

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,7 +2,10 @@ const mongoose=require("mongoose");
 require("dotenv").config();
 
 function connectDatabase(){
-    mongoose.connect(process.env.DATABASE_URL)
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(()=>{console.log("Database Connection is Successfull")})
     .catch((error)=>{
         console.log("Database Connection is failed",error);
