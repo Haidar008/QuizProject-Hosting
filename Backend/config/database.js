0bd04f434d19a74e817598d92c2f09c9f08acc03
parwez <parwezhaidar555@gmail,com>
2024-03-22 05:55:24 +0530

    Fourth Change

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -4,14 +4,10 @@ require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
 exports.connect = () => {
-    mongoose.connect(DATABASE_URL,{
-        useNewUrlparser: true,
-        useUnifiedTopology: true,
-    })
+    mongoose.connect(DATABASE_URL)
     .then(console.log(`DB Connection Success`))
 		.catch((err) => {
 			console.log(`DB Connection Failed`);
-			console.log(err);
 			process.exit(1);
 		});
 }
